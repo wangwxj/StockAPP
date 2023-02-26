45a60eda138a9b41d66070c5f313f76aad35954c
wangwxj <31007036+wangwxj@users.noreply.github.com>
2023-02-26 12:42:46 +0800

    Update StockAPP.py

--- StockAPP.py
+++ StockAPP.py
@@ -358,7 +358,7 @@ Chart=alt.Chart(source).mark_bar(width=10).encode(
     y='sum(val)',
     color="date",
     column='reason'
-).configure_view(continuousWidth=5)
+).configure_view(continuousWidth=400)
 
 st.altair_chart(Chart,use_container_width=False)
 
