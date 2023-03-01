44e5968733611d2f911895341a5e0981d240138f
wang <wangwxj@qq.com>
2023-03-01 20:14:05 +0800

    3.0

--- qingxu.py
+++ qingxu.py
@@ -52,7 +52,7 @@ data = dfffzb.query(
 )
 
 
-Chartzb = alt.Chart(data).mark_bar(color="red").encode(x='trade_date:N',y='sum(pct_chg)')
+Chartzb = alt.Chart(data).mark_bar(color="red").encode(x='trade_date:T',y='sum(pct_chg)')
 Chartzb = Chartzb.configure_axis(title='')
 st.altair_chart(Chartzb,use_container_width=True)
 st.dataframe(data)
